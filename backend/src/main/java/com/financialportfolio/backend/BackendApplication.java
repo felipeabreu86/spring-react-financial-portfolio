4961699f63a45d10f8e27f6d98a76f0397b18a6e
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-09-28 09:14:24 -0300

    update pom.xml

--- backend/src/main/java/com/financialportfolio/backend/BackendApplication.java
+++ backend/src/main/java/com/financialportfolio/backend/BackendApplication.java
@@ -2,12 +2,16 @@ package com.financialportfolio.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
+@EnableCaching
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
